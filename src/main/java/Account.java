56b62565163670139b3401ec6c24dfce2aa964c7
MakeLord95 <juhani.markus@gmail.com>
2024-02-02 10:11:27 +0200

    Account

---
+++ src/main/java/Account.java
@@ -0,0 +1,24 @@
+public class Account {
+    private double balance;
+
+    public Account() {
+        balance = 0;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public double withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            return amount;
+        } else {
+            return 0.0;
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
---
+++ src/test/java/AccountTest.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AccountTest {
+
+    @Test
+    public void depositIncreasesBalance() {
+        Account account = new Account();
+        account.deposit(100.0);
+        assertEquals(100.0, account.getBalance());
+    }
+
+    @Test
+    public void withdrawDecreasesBalance() {
+        Account account = new Account();
+        account.deposit(200.0);
+        account.withdraw(100.0);
+        assertEquals(100.0, account.getBalance());
+    }
+
+    @Test
+    public void withdrawReturnsZeroWhenInsufficientBalance() {
+        Account account = new Account();
+        account.deposit(50.0);
+        double withdrawnAmount = account.withdraw(100.0);
+        assertEquals(0.0, withdrawnAmount);
+    }
+
+    @Test
+    public void withdrawDoesNotChangeBalanceWhenInsufficientBalance() {
+        Account account = new Account();
+        account.deposit(50.0);
+        account.withdraw(100.0);
+        assertEquals(50.0, account.getBalance());
+    }
+}

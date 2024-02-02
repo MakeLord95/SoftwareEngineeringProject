694a1eb6d30e82d88e527d22ea457b89229198cd
make <juhani.markus@gmail.com>
2024-02-02 09:15:38 +0200

    Initial commit

---
+++ src/main/java/BinarySearch.java
@@ -0,0 +1,29 @@
+public class BinarySearch {
+    public static int search(int[] a, int x) {
+        int left = 0;
+        int right = a.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (a[mid] == x) {
+                return mid;
+            } else if (a[mid] < x) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static int checkedSearch(int[] a, int x) {
+        int index = search(a, x);
+        if (index != -1) {
+            return index;
+        } else {
+            throw new IllegalArgumentException("Element not found in the array");
+        }
+    }
+}
---
+++ src/test/java/BinarySearchTest.java
@@ -0,0 +1,53 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class BinarySearchTest {
+
+    @Test
+    void searchFindsElementInArray() {
+        int[] array = {1, 2, 3, 4, 5};
+        int x = 3;
+        int expected = 2;
+        assertEquals(expected, BinarySearch.search(array, x));
+    }
+
+    @Test
+    void searchReturnsMinusOneWhenElementNotInArray() {
+        int[] array = {1, 2, 3, 4, 5};
+        int x = 6;
+        int expected = -1;
+        assertEquals(expected, BinarySearch.search(array, x));
+    }
+
+    @Test
+    void searchHandlesEmptyArray() {
+        int[] array = {};
+        int x = 1;
+        int expected = -1;
+        assertEquals(expected, BinarySearch.search(array, x));
+    }
+
+    @Test
+    void checkedSearchFindsElementInArray() {
+        int[] array = {1, 2, 3, 4, 5};
+        int x = 3;
+        int expected = 2;
+        assertEquals(expected, BinarySearch.checkedSearch(array, x));
+    }
+
+    @Test
+    void checkedSearchThrowsExceptionWhenElementNotInArray() {
+        int[] array = {1, 2, 3, 4, 5};
+        int x = 6;
+        assertThrows(IllegalArgumentException.class, () -> BinarySearch.checkedSearch(array, x));
+    }
+
+    @Test
+    void checkedSearchThrowsExceptionWhenArrayIsEmpty() {
+        int[] array = {};
+        int x = 1;
+        assertThrows(IllegalArgumentException.class, () -> BinarySearch.checkedSearch(array, x));
+    }
+}

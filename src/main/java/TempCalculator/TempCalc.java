b3640c685e8dafa2b20f7c6eb2117202a5c5cb86
MakeLord95 <juhani.markus@gmail.com>
2024-02-22 00:12:05 +0200

    TempCalc

---
+++ src/main/java/TempCalculator/TempCalc.java
@@ -0,0 +1,37 @@
+package TempCalculator;
+
+public class TempCalc {
+
+    public static float KelvinToCel(float kelvin) {
+        return kelvin - 273.15f;
+    }
+
+    public static float CelToKelvin(float cel) {
+        return cel + 273.15f;
+    }
+
+    public static float FarToCel(float far) {
+        return (far - 32) * 5 / 9;
+    }
+
+    public static float CelToFar(float cel) {
+        return (cel * 9 / 5) + 32;
+    }
+
+    public static float KelvinToFar(float kelvin) {
+        return CelToFar(KelvinToCel(kelvin));
+    }
+
+    public static float FarToKelvin(float far) {
+        return CelToKelvin(FarToCel(far));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(KelvinToCel(15f));
+        System.out.println(CelToKelvin(23.5f));
+        System.out.println(FarToCel(105f));
+        System.out.println(CelToFar(23.5f));
+        System.out.println(KelvinToFar(10f));
+        System.out.println(FarToKelvin(91f));
+    }
+}
---
+++ src/test/java/TempCalculator/TempCalcTest.java
@@ -0,0 +1,38 @@
+
+package TempCalculator;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TempCalcTest {
+
+    @Test
+    public void testKelvinToCel() {
+        assertEquals(-258.15f, TempCalc.KelvinToCel(15f), 0.01);
+    }
+
+    @Test
+    public void testCelToKelvin() {
+        assertEquals(296.65, TempCalc.CelToKelvin(23.5f), 0.01);
+    }
+
+    @Test
+    public void testFarToCel() {
+        assertEquals(40.56f, TempCalc.FarToCel(105f), 0.01);
+    }
+
+    @Test
+    public void testCelToFar() {
+        assertEquals(74.3, TempCalc.CelToFar(23.5f), 0.01);
+    }
+
+    @Test
+    public void testKelvinToFar() {
+        assertEquals(-441.67, TempCalc.KelvinToFar(10f), 0.01);
+    }
+
+    @Test
+    public void testFarToKelvin() {
+        assertEquals(305.928, TempCalc.FarToKelvin(91f), 0.01);
+    }
+}
